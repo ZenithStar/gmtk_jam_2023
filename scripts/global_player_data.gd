ab54f8fbb09afed78f3a706f3ff19ab50a856151
Angela <aml2732@utexas.edu>
2023-07-08 11:25:41 -0500

    Basic save (#11)

--- scripts/global_player_data.gd
+++ scripts/global_player_data.gd
@@ -1,7 +1,26 @@
 extends Node
 
-var max_level_reached = 0;
+var max_level_reached = -1;
 
+
+func load_values():
+	var config = ConfigFile.new()
+	var err = config.load("user://rodreversal.cfg")
+	if err != OK:
+		return
+	max_level_reached = config.get_value("global_player_data", "max_level_reached")
+
+func save_values():
+	var config = ConfigFile.new()
+	config.set_value("global_player_data", "max_level_reached", max_level_reached)
+	config.save("user://rodreversal.cfg")
+	
+func _enter_tree():
+	load_values()
+
+func _exit_tree():
+	save_values()
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
--- scripts/new_game.gd
+++ scripts/new_game.gd
@@ -4,5 +4,6 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _pressed():
 	GlobalPlayerData.max_level_reached = 0
+	GlobalPlayerData.save_values()
 	SceneTransition.change_scene(GlobalConstants.level_lookup[0])
 
