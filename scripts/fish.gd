d0838e23f7ede86792342ad12af23c5126cd39da
robz <rjnevels@gmail.com>
2023-07-08 11:01:22 -0500

    update fishics with y component and dampening

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -10,26 +10,34 @@ func _process(_delta):
 
 
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity") * ProjectSettings.get_setting("physics/2d/default_gravity_vector")
-var MAX_THRUST = 300
-
+@export var MAX_THRUST = 5000
+@export var MAX_VELOCITY = 1000
 
 func _integrate_forces(state):
 	var buoyancy = Vector2.ZERO
+	var thrust = Vector2.ZERO
+
 	#var water = get_parent().get_node('ParallaxForeground/ParallaxLayer2/Water')
 	#if water.overlaps_body(self):
-	if position.y > -490:
-		buoyancy = -1.001 * GRAVITY * mass
+	var is_in_water = position.y > -450
+	if is_in_water:
+		buoyancy = -1.01 * GRAVITY * mass
 
-	var thrust_x = 0
-	if Input.is_action_pressed("right"):
-		thrust_x = 1
-	elif Input.is_action_pressed("left"):
-		thrust_x = -1
-	var thrust = Vector2(1, 0) * thrust_x * MAX_THRUST
+		if Input.is_action_pressed("right"):
+			thrust.x = 1
+		elif Input.is_action_pressed("left"):
+			thrust.x = -1
+		if Input.is_action_pressed("down"):
+			thrust.y = .4
+		elif Input.is_action_pressed("up"):
+			thrust.y = -.4
+		thrust = thrust.normalized() * MAX_THRUST
 	
-	if Input.is_action_pressed("up"):
-		thrust = thrust.rotated(-sign(thrust_x) * PI/4)
-	elif Input.is_action_pressed("down"):
-		thrust = thrust.rotated(sign(thrust_x) * PI/4)
+	var dampening = Vector2.ZERO
+	if thrust.length() == 0:
+		dampening = -2 * state.linear_velocity
+	else:
+		# calculate dampening for desired terminal velocity
+		dampening = -MAX_THRUST / (mass * MAX_VELOCITY) * state.linear_velocity
 
-	state.apply_force(thrust + buoyancy)
+	state.apply_force(dampening + thrust + buoyancy)
