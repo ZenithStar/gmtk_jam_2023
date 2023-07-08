a3239ce7797dacb8883c8c01836271aeca1c06d0
Frank Weng <lpvvnnoobs@gmail.com>
2023-07-07 22:55:28 -0500

    Added scene transition base that can be improved. Moved shaders to their own folder

--- StartGame.gd
+++ StartGame.gd
@@ -3,5 +3,5 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _pressed():
-	get_tree().change_scene_to_file("res://scenes/test_level.tscn")
+	SceneTransition.change_scene("res://scenes/test_level.tscn")
 
