e2dcaade57f5844a52322942fec3edfa6bb4772b
Frank Weng <lpvvnnoobs@gmail.com>
2023-07-08 11:44:45 -0500

    Still trying to figure out the area collision discrepency

--- scripts/Buoyant.gd
+++ scripts/Buoyant.gd
@@ -1,16 +1,16 @@
 extends RigidBody2D
-class_name Bouyant
+class_name Buoyant
 
-@export var bouyancy_factor : float = 2.0
+@export var buoyant_factor : float = 1.0
 var submerged = false
 var gravity_vector : Vector2 = ProjectSettings.get_setting("physics/2d/default_gravity_vector")
 var gravity_magnitude : int = ProjectSettings.get_setting("physics/2d/default_gravity")
-var bouyant_force = - bouyancy_factor * gravity_vector * gravity_magnitude * mass
+var buoyant_force = - buoyant_factor * gravity_vector * gravity_magnitude * mass
 
 func set_submerged(val:bool):
 	submerged = val
 	if submerged:
-		add_constant_central_force(bouyant_force)
+		add_constant_central_force(buoyant_force)
 	else:
 		constant_force = Vector2(0, 0)
 	
--- scripts/Water.gd
+++ scripts/Water.gd
@@ -1,20 +1,15 @@
-@tool
 extends Area2D
 class_name Water
 
-@export var collision_size : Vector2 = Vector2(128,128)
-
 func _ready():
-	$CollisionShape2D.shape.size = collision_size
-	$Sprite2D.apply_scale( collision_size / Vector2(128,128) )
-	
+	$Sprite2D.apply_scale( $CollisionShape2D.shape.size / Vector2(128,128) )
 
 func _on_body_entered(body):
 	print(body, 'entered water')
-	if body is Bouyant:
+	if body is Buoyant:
 		body.set_submerged(true)
 
 func _on_body_exited(body):
 	print(body, 'exited water')
-	if body is Bouyant:
+	if body is Buoyant:
 		body.set_submerged(false)
