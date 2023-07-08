300386f13a67d92298a74a68a9076ca5ea50d763
robz <rjnevels@gmail.com>
2023-07-07 22:54:27 -0500

    make fish a rigid body again

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -1,26 +1,27 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
-const speed = 300.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_left = false
-
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("right"):
 		$Sprite2D.flip_h = false
 	elif Input.is_action_pressed("left"):
 		$Sprite2D.flip_h = true
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
 
-func _physics_process(delta):
-	get_input()
+var MAX_THRUST = 10000
+var thrust_x = 0
+func _integrate_forces(state):
+	if Input.is_action_pressed("right"):
+		thrust_x += 0.01
+	if Input.is_action_pressed("left"):
+		thrust_x -= 0.01
+	thrust_x = clamp(thrust_x, -1, 1)
+	var thrust = Vector2(1, 0) * thrust_x * MAX_THRUST
 	
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if Input.is_action_pressed("up"):
+		thrust = thrust.rotated(-sign(thrust_x) * PI/4)
+	elif  Input.is_action_pressed("down"):
+		thrust = thrust.rotated(sign(thrust_x) * PI/4)
 
-	move_and_slide()
+	state.apply_force(thrust)
