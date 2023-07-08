a40d7000dded7ad407caa83b6f1f77832700257b
robz <rjnevels@gmail.com>
2023-07-08 13:51:50 -0500

    improve fishics

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -1,12 +1,7 @@
 extends RigidBody2D
 
 
-var facing_left = false
-func _process(_delta):
-	if Input.is_action_pressed("right"):
-		$Sprite2D.flip_h = false
-	elif Input.is_action_pressed("left"):
-		$Sprite2D.flip_h = true
+@onready var water = get_parent().get_node('Foreground/Water')
 
 
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity") * ProjectSettings.get_setting("physics/2d/default_gravity_vector")
@@ -14,31 +9,42 @@ var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity") * Projec
 @export var MAX_VELOCITY = 1000
 
 
-@onready var water = get_parent().get_node('Foreground/Water')
-
 func _integrate_forces(state):
-	var buoyancy = Vector2.ZERO
-	var thrust = Vector2.ZERO
-
 	var is_in_water = water.overlaps_body(self)
-	if is_in_water:
-		buoyancy = -1.01 * GRAVITY * mass
-
-		if Input.is_action_pressed("right"):
-			thrust.x = 1
-		elif Input.is_action_pressed("left"):
-			thrust.x = -1
-		if Input.is_action_pressed("down"):
-			thrust.y = .4
-		elif Input.is_action_pressed("up"):
-			thrust.y = -.4
-		thrust = thrust.normalized() * MAX_THRUST
 	
-	var dampening = Vector2.ZERO
-	if thrust.length() == 0:
-		dampening = -2 * state.linear_velocity
+	var buoyancy = -1.01 * GRAVITY * mass if is_in_water else Vector2.ZERO
+
+	var thrust = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		thrust.x = 1
+	elif Input.is_action_pressed("left"):
+		thrust.x = -1
+	if Input.is_action_pressed("down"):
+		thrust.y = .4
+	elif Input.is_action_pressed("up"):
+		thrust.y = -.4
+	thrust = thrust.normalized() * MAX_THRUST
+	if !is_in_water:
+		# can't move as fast out of water, and remove vertical thrust
+		thrust *= Vector2(.1, 0)
+
+	var dampening_factor = 0
+	if !is_in_water:
+		# don't dampen speed while in air
+		dampening_factor = 0
+	elif thrust.length() == 0:
+		# slow down quickly when not actively controlling
+		dampening_factor = 2
 	else:
-		# calculate dampening for desired terminal velocity
-		dampening = -MAX_THRUST / (mass * MAX_VELOCITY) * state.linear_velocity
+		# set dampening based on desired terminal velocity
+		dampening_factor = MAX_THRUST / (mass * MAX_VELOCITY)
+	var dampening = -state.linear_velocity * dampening_factor
+	
+	if thrust.length() != 0 or !is_in_water:
+		if state.linear_velocity.x < 0:
+			$Sprite2D.flip_h = true
+		elif state.linear_velocity.x > 0:
+			$Sprite2D.flip_h = false
+		$Sprite2D.rotation = state.linear_velocity.angle() + (PI if $Sprite2D.flip_h else 0)
 
 	state.apply_force(dampening + thrust + buoyancy)
