fef40921914e363cf5a77972e7469f0e13046c95
unknown <aml2732@utexas.edu>
2023-07-08 14:40:36 -0500

    remove print statements and add resume button

--- scripts/level_select_button.gd
+++ scripts/level_select_button.gd
@@ -11,4 +11,4 @@ func _process(delta):
 	pass
 
 func _pressed():
-	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
+	get_tree().change_scene_to_file("res://levels/level_select.tscn")
