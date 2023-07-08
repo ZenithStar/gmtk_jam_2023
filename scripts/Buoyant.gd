18eb21c1a343570e488e0fbe5a1cc4078917b57d
Frank Weng <lpvvnnoobs@gmail.com>
2023-07-08 12:11:54 -0500

    Fixed the water collision problem

--- scripts/Buoyant.gd
+++ scripts/Buoyant.gd
@@ -3,14 +3,15 @@ class_name Buoyant
 
 @export var buoyant_factor : float = 1.0
 var submerged = false
-var gravity_vector : Vector2 = ProjectSettings.get_setting("physics/2d/default_gravity_vector")
-var gravity_magnitude : int = ProjectSettings.get_setting("physics/2d/default_gravity")
-var buoyant_force = - buoyant_factor * gravity_vector * gravity_magnitude * mass
+@onready var gravity_vector : Vector2 = ProjectSettings.get_setting("physics/2d/default_gravity_vector")
+@onready var gravity_magnitude : int = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var buoyant_force = - buoyant_factor * gravity_vector * gravity_magnitude * mass
 
 func set_submerged(val:bool):
 	submerged = val
 	if submerged:
 		add_constant_central_force(buoyant_force)
+		print(name," buoyant_force added ", buoyant_force)
 	else:
 		constant_force = Vector2(0, 0)
 	
--- scripts/Water.gd
+++ scripts/Water.gd
@@ -3,6 +3,7 @@ class_name Water
 
 func _ready():
 	$Sprite2D.apply_scale( $CollisionShape2D.shape.size / Vector2(128,128) )
+	print($Sprite2D.scale)
 
 func _on_body_entered(body):
 	print(body, 'entered water')
