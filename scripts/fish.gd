4b1f42b9c0b4c1048331e30c85d531c6b222f3b5
robz <rjnevels@gmail.com>
2023-07-07 23:11:15 -0500

    adjust thrust

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -9,19 +9,19 @@ func _process(_delta):
 		$Sprite2D.flip_h = true
 
 
-var MAX_THRUST = 10000
+var MAX_THRUST = 5000
 var thrust_x = 0
 func _integrate_forces(state):
 	if Input.is_action_pressed("right"):
-		thrust_x += 0.01
+		thrust_x += 0.1
 	if Input.is_action_pressed("left"):
-		thrust_x -= 0.01
+		thrust_x -= 0.1
 	thrust_x = clamp(thrust_x, -1, 1)
 	var thrust = Vector2(1, 0) * thrust_x * MAX_THRUST
 	
 	if Input.is_action_pressed("up"):
 		thrust = thrust.rotated(-sign(thrust_x) * PI/4)
-	elif  Input.is_action_pressed("down"):
+	elif Input.is_action_pressed("down"):
 		thrust = thrust.rotated(sign(thrust_x) * PI/4)
 
 	state.apply_force(thrust)
