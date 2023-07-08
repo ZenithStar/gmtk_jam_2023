344e8f1de0447c860b9b28b2bfeb0c2e0ef8133d
Angela <aml2732@utexas.edu>
2023-07-08 14:41:50 -0500

    remove print statements and add resume button (#16)

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -5,16 +5,22 @@ var paused = false
 
 func _ready():
 	set_paused(paused)
+	get_node("PauseOverlay/ResumeGame").pressed.connect(self.resume_game)
+	get_node("PauseOverlay/ReturnToMainMenu").pressed.connect(self.return_to_main_menu)
 	
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
-		print("got to pause button")
 		self.set_paused(!paused)
 		get_viewport().set_input_as_handled()
 
 func set_paused(is_paused):
-	print("got to set paused")
-	print(is_paused)
 	paused = is_paused
 	scene_tree_ref.paused = is_paused
 	get_node("PauseOverlay").visible = is_paused
+	
+func resume_game():
+	set_paused(false)
+	
+func return_to_main_menu():
+	scene_tree_ref.paused = false
+	SceneTransition.change_scene("res://levels/title_screen.tscn")
