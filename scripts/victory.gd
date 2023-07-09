0a15b47974b6cad4de3232d502bd373899d74bf5
Angela <aml2732@utexas.edu>
2023-07-09 00:34:46 -0500

    Some really wild victory screen (#25)

--- scripts/victory.gd
+++ scripts/victory.gd
@@ -4,6 +4,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("fly_across_screen")
+	$AnimationPlayer2.play("barral_fly")
+	$AnimationPlayer3.play("cooler_fly")
 	get_node("Timer").timeout.connect(self.timeout)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
